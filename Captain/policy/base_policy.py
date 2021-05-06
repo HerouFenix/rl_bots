fab1a1cfbf78cd1687a8e37b8be142009da63c9d
Tiago Melo <tiagomelo@ua.pt>
2021-05-06 11:27:17 +0100

    Importing mechanics and actions - about to remove tests

---
+++ Captain/policy/base_policy.py
@@ -0,0 +1,53 @@
+import math
+from typing import Callable
+
+from action.collect_boost import CollectBoost
+from action.kickoff import Kickoff
+
+class BasePolicy:
+    """ This is where the agent will pick his actions and decide the strategy to follow.
+        This is the highest level of abstraction in the project:
+            Should return an action, an action returns a mechanic, and mechanics return controls.
+        The policy always refers to a Capitao, since it is also an action to assign other actions.
+    """
+    def __init__(self, agent, rendering_enabled=False):
+        self.agent = agent
+        self.kickoff_action = Kickoff(agent, rendering_enabled)
+        self.action_loop = self.create_action_loop()
+
+    def get_controls(self, game_data):
+        return self.get_action(game_data).get_controls(game_data)
+
+    def get_action(self, game_data):
+        ball_loc = game_data.ball.location
+        kickoff = math.sqrt(ball_loc[0] ** 2 + ball_loc[1] ** 2) < 1
+
+        if kickoff:
+            # reset the action loop
+            self.action_loop = self.create_action_loop()
+            return self.kickoff_action
+        else:
+            return self.action_loop.send(game_data)
+
+    @initialize_generator
+    def create_action_loop(self) -> Generator[BaseAction, GameData, None]:
+        game_data = yield
+
+        while True:
+            # choose action to do
+            action = CollectBoost(self.agent)
+
+            # use action until it is finished
+            while not action.finished and not action.failed:
+                game_data = yield action
+
+
+def initialize_generator(func: Callable):
+    """initializes a generator by calling next on it once"""
+
+    def result(self):
+        gen = func(self)
+        next(gen)
+        return gen
+
+    return result
